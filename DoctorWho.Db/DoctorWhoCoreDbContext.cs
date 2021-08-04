9b424e10fb9a32011a7eab24f6896b8d38f5bc93
Ibrahim Masri <msri.ibraheem@gmail.com>
2021-08-04 18:37:19 +0300

    Create a DbContext for the added domain classes

--- DoctorWho.Db/DoctorWhoCoreDbContext.cs
+++ DoctorWho.Db/DoctorWhoCoreDbContext.cs
@@ -1,9 +1,25 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using DoctorWho.Db.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace DoctorWho.Db
 {
     public class DoctorWhoCoreDbContext : DbContext
     {
-        
+        public DbSet<Episode> Episodes { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Companion> Companions { get; set; }
+        public DbSet<Enemy> Enemies { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+
+            );
+            base.OnConfiguring(optionsBuilder);
+        }
+
+
     }
 }
