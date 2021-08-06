9bcdf08efce20429c6f9d57974b4915dd8bd1b95
Ibrahim Masri <msri.ibraheem@gmail.com>
2021-08-06 23:00:26 +0300

    Add seeding code to DoctorWhoCoreDbContext.cs

--- DoctorWho.Db/DoctorWhoCoreDbContext.cs
+++ DoctorWho.Db/DoctorWhoCoreDbContext.cs
@@ -1,6 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using DoctorWho.Db.Domain;
+using DoctorWho.Db.Utilities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace DoctorWho.Db
 {
@@ -15,14 +25,13 @@ namespace DoctorWho.Db
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(
-
             );
             base.OnConfiguring(optionsBuilder);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Enemy>().HasMany(e => e.Episodes).WithMany(e => e.Enemies).UsingEntity(j =>
+            GetJoinEntityBuilder<Enemy, Episode>(modelBuilder).UsingEntity(j =>
             {
                 j.ToTable("EpisodeEnemy");
                 j.Property("EpisodesEpisodeId").HasColumnName("EpisodeId");
@@ -31,7 +40,7 @@ namespace DoctorWho.Db
                 j.HasKey("EpisodeEnemyId");
             });
 
-            modelBuilder.Entity<Companion>().HasMany(c => c.Episodes).WithMany(e => e.Companions).UsingEntity(j =>
+            GetJoinEntityBuilder<Companion, Episode>(modelBuilder).UsingEntity(j =>
             {
                 j.ToTable("EpisodeCompanion");
                 j.Property("EpisodesEpisodeId").HasColumnName("EpisodeId");
@@ -39,7 +48,106 @@ namespace DoctorWho.Db
                 j.Property<int>("EpisodeCompanionId");
                 j.HasKey("EpisodeCompanionId");
             });
+
+            SeedModel(modelBuilder);
             base.OnModelCreating(modelBuilder);
+        }
+
+        private void SeedModel(ModelBuilder modelBuilder)
+        {
+            SeedEntities(modelBuilder);
+            SeedJoinEntities(modelBuilder);
+        }
+
+        private void SeedEntities(ModelBuilder modelBuilder)
+        {
+            SeedEntity<Author>(modelBuilder);
+            SeedEntity<Enemy>(modelBuilder);
+            SeedEntity<Doctor>(modelBuilder);
+            SeedEntity<Companion>(modelBuilder);
+            SeedEntity<Episode>(modelBuilder);
+        }
+
+        private void SeedEntity<T>(ModelBuilder modelBuilder) where T : class
+        {
+            string fakeDataFilePath = FakeDataGenerator.GetFilePathForTypeFakes<T>();
+
+            string listJson = File.ReadAllText(fakeDataFilePath);
+            List<T> entities = JsonSerializer.Deserialize<List<T>>(listJson) ?? new List<T>();
+
+            modelBuilder.Entity<T>().HasData(entities);
+        }
+
+        private void SeedJoinEntities(ModelBuilder modelBuilder)
+        {
+            string enemyEpisodeFakeDataFilePath = FakeDataGenerator.GetFilePathForJoinTypeFakes<Enemy, Episode>();
+
+            string enemyEpisodeJsonList = File.ReadAllText(enemyEpisodeFakeDataFilePath);
+            List<IDictionary<string, int>> enemyEpisodeEntities =
+                JsonSerializer.Deserialize<List<IDictionary<string, int>>>(enemyEpisodeJsonList) ??
+                new List<IDictionary<string, int>>();
+            var mappedEnemyEpisodes
+                = enemyEpisodeEntities.Select(e => new
+                {
+                    EpisodesEpisodeId = e["EpisodesEpisodeId"],
+                    EnemiesEnemyId = e["EnemiesEnemyId"],
+                    EpisodeEnemyId = e["EpisodeEnemyId"],
+                });
+            
+            GetJoinEntityBuilder<Enemy, Episode>(modelBuilder)
+                .UsingEntity(j => j.HasData(mappedEnemyEpisodes));
+
+
+            string companionEpisodeFakeDataFilePath =
+                FakeDataGenerator.GetFilePathForJoinTypeFakes<Companion,Episode>();
+
+            string companionEpisodeJsonList = File.ReadAllText(companionEpisodeFakeDataFilePath);
+            List<IDictionary<string, int>> companionEpisodeEntities =
+                JsonSerializer.Deserialize<List<IDictionary<string, int>>>(companionEpisodeJsonList) ??
+                new List<IDictionary<string, int>>();
+            var mappedCompanionEpisodes
+                = companionEpisodeEntities.Select(e => new
+                {
+                    EpisodesEpisodeId = e["EpisodesEpisodeId"],
+                    CompanionsCompanionId = e["CompanionsCompanionId"],
+                    EpisodeCompanionId = e["EpisodeCompanionId"]
+                });
+            
+            GetJoinEntityBuilder<Companion, Episode>(modelBuilder)
+                .UsingEntity(j => j.HasData(mappedCompanionEpisodes));
+        }
+
+        private static CollectionCollectionBuilder<TSecondEntityType, TFirstEntityType>
+            GetJoinEntityBuilder<TFirstEntityType, TSecondEntityType>(ModelBuilder modelBuilder)
+            where TFirstEntityType : class where TSecondEntityType : class
+        {
+            var firstEntityCollectionProperty = GetNavigationCollectionProperty<TFirstEntityType, TSecondEntityType>();
+            var secondEntityCollectionProperty = GetNavigationCollectionProperty<TSecondEntityType, TFirstEntityType>();
+
+            var entityBuilder = modelBuilder.Entity<TFirstEntityType>()
+                .HasMany<TSecondEntityType>(
+                    firstEntityCollectionProperty.Name
+                )
+                .WithMany(
+                    secondEntityCollectionProperty.Name
+                );
+
+            return entityBuilder;
+        }
+
+        private static PropertyInfo GetNavigationCollectionProperty<TFirstEntityType, TSecondEntityType>()
+            where TFirstEntityType : class where TSecondEntityType : class
+        {
+            var properties = typeof(TFirstEntityType).GetProperties();
+
+            PropertyInfo collectionProperty = null;
+            foreach (var property in properties)
+            {
+                if (property.PropertyType == typeof(ICollection<TSecondEntityType>))
+                    collectionProperty = property;
+            }
+
+            return collectionProperty;
         }
     }
 }
